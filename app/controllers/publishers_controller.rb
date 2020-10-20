b2899d747ae5674d435a94523d889d161932ab90
markoristic99 <marko.ristic99@gmail.com>
2020-10-20 23:01:53 +0200

    Delete and Edit

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -15,18 +15,29 @@ class PublishersController < ApplicationController
   end
 
   def update
+    @publisher = Publisher.find(params[:id])
+    @publisher.update(publisher_params)
+
+    flash[:notice] = 'Publisher updated'
+
+    redirect_to publishers_path
   end
 
   def edit
+    @publisher = Publisher.find(params[:id])
   end
 
   def destroy
+    @publisher = Publisher.find(params[:id])
+    @publisher.destroy 
+
+    flash[:notice] = 'Publisher removed'
+
+    redirect_to publishers_path
   end
 
   def index
-  end
-
-  def show
+    @publishers = Publisher.all
   end
 
   private
